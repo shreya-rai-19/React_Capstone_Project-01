40bd709c99b71cedcc34614629beee454f5626a1
Shreya Rai <shreya.rai8091@gmail.com>
2023-10-01 13:41:56 +0530

    added registration page

--- super-app/src/App.js
+++ super-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import RegistrationForm from './components/RegistrationForm';
 function App() {
   return (
 
   <>
-    
+    <RegistrationForm/>
   </>
     );
 }
---
+++ super-app/src/components/RegistrationForm.js
@@ -0,0 +1,186 @@
+// import React, {useState} from 'react'
+
+// const RegistrationForm = () => {
+//   return (
+//     <div>RegistrationForm</div>
+//   )
+// }
+
+// export default RegistrationForm
+
+// RegistrationForm.js
+import React, { useState } from "react";
+import "../styles/Reg.css";
+function RegistrationForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    mobile: "",
+    email: "",
+    acceptedTerms: false,
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+
+    setFormData({
+      ...formData,
+      [name]: newValue,
+    });
+
+    // Clear the error message when the field is changed
+    setErrors({
+      ...errors,
+      [name]: "",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validation
+    const newErrors = {};
+    if (!formData.name) {
+      newErrors.name = "Field is required";
+    }
+    if (!formData.username) {
+      newErrors.username = "Field is required";
+    }
+    if (!formData.mobile) {
+      newErrors.mobile = "Field is required";
+    }
+    if (!formData.email) {
+      newErrors.email = "Field is required";
+    }
+    if (!formData.acceptedTerms) {
+      newErrors.acceptedTerms = "Check this box if you want to proceed";
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      // Registration successful - Save data to local storage
+      localStorage.setItem("registrationData", JSON.stringify(formData));
+      alert("Registration successful! Data saved in local storage.");
+      // Reset the form
+      setFormData({
+        name: "",
+        username: "",
+        mobile: "",
+        email: "",
+        acceptedTerms: false,
+      });
+    }
+  };
+
+  return (
+    <div className="reg">
+      <div className="left-image">
+        <div className="disover-text">
+          <label className="left-text">Discover new things on Superapp</label>
+        </div>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-content">
+          <div className="wrap-up">
+            <label className="heading-super">Super app</label>
+            <br />
+            <label className="header-text">Create your new account</label>
+            <div>
+              <input
+                className="fields"
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+              {errors.name && <div className="error">{errors.name}</div>}
+            </div>
+
+            <div>
+              <input
+                className="fields"
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+              {errors.username && (
+                <div className="error">{errors.username}</div>
+              )}
+            </div>
+
+            <div>
+              <input
+                className="fields"
+                type="text"
+                name="mobile"
+                placeholder="Mobile"
+                value={formData.mobile}
+                onChange={handleChange}
+              />
+              {errors.mobile && <div className="error">{errors.mobile}</div>}
+            </div>
+
+            <div>
+              <input
+                className="fields"
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              {errors.email && <div className="error">{errors.email}</div>}
+            </div>
+
+            <div>
+              <br />
+              <label className="checkbox">
+                <input
+                  className="check"
+                  type="checkbox"
+                  name="acceptedTerms"
+                  checked={formData.acceptedTerms}
+                  onChange={handleChange}
+                />{" "}
+                Share my registration data with Superapp
+              </label>
+              {errors.acceptedTerms && (
+                <div className="error">{errors.acceptedTerms}</div>
+              )}
+            </div>
+
+            <br />
+            <div className="tnc">
+              <label className="text">
+                By clicking on Sign up. you agree to Superapp{" "}
+                <span>Terms and Conditions</span> of Use
+              </label>
+            </div>
+            <br />
+            <div className="tnc">
+              <label className="text">
+                To learn more about how Superapp collects, uses, shares and
+                protects your personal data please head Superapp{" "}
+                <span>Privacy Policy</span>
+              </label>
+            </div>
+
+            <button className="signup" type="submit">
+              SIGN UP
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default RegistrationForm;
